6cd3273320b459b69d5008b74e8d2da92775bb27
Jorge Fabila <jorgefabila1997@gmail.com>
2025-09-12 19:55:41 +0200

    correcciones checkpoint

--- flcore/models/nn/client.py
+++ flcore/models/nn/client.py
@@ -32,40 +32,50 @@ from pathlib import Path
 
 from collections import OrderedDict
 
-from train import train
-from test import test
-from utils import Parameters
 from torch.utils.data import TensorDataset, DataLoader
+import torch.optim as optim
+import torch.nn as nn
 
-from basic_nn import BasicNN
+from flcore.models.nn.basic_nn import BasicNN
 
 class FlowerClient(fl.client.NumPyClient):
     def __init__(self, config, data):
         self.params = config
+        self.batch_size = config["batch_size"]
+        self.lr = config["lr"]
+        self.epochs = config["local_epochs"]
 
+        print("MODELS::NN:CLIENT::INIT")
         if torch.cuda.is_available() and self.params["device"] == 'cuda':
-            device = torch.device('cuda')
+            self.device = torch.device('cuda')
         else:
-            device = torch.device("cpu")
+            self.device = torch.device("cpu")
 
         (self.X_train, self.y_train), (self.X_test, self.y_test) = data
 
-        self.X_train = torch.tensor(self.X_train, dtype=torch.float32)
-        self.y_train = torch.tensor(self.y_train, dtype=torch.long)
-        self.X_test = torch.tensor(self.X_test, dtype=torch.float32)
-        self.y_test = torch.tensor(self.y_test, dtype=torch.long)
+        print("ARREGLOS xtrain", self.X_train.shape)
+        print("ARREGLOS ytrain", self.y_train.shape)
+        print("ARREGLOS xtest", self.X_test.shape)
+        print("ARREGLOS ytest", self.y_test.shape)
+
+        print(" xtrain", type(self.X_train))
+        print("ARREGLOS ytrain", type(self.y_train))
+        print("ARREGLOS xtest", type(self.X_test))
+        print("ARREGLOS ytest", self.y_test)
+        
+        self.X_train = torch.tensor(self.X_train.values, dtype=torch.float32)
+        self.y_train = torch.tensor(self.y_train.values, dtype=torch.float32)
+        self.X_test = torch.tensor(self.X_test.values, dtype=torch.float32)
+        self.y_test = torch.tensor(self.y_test.values, dtype=torch.float32)
 
         train_ds = TensorDataset(self.X_train, self.y_train)
         test_ds = TensorDataset(self.X_test, self.y_test)
         self.train_loader = DataLoader(train_ds, batch_size=self.batch_size, shuffle=True)
         self.test_loader = DataLoader(test_ds, batch_size=self.batch_size, shuffle=False)
 
-        self.batch_size = config["batch_size"]
-        self.lr = config["lr"]
-        self.epochs = config["local_epochs"]
-
-        self.model = BasicNN( config["n_feats"], config["n_out"], config["dropout_p"] ).to(device)
-        self.criterion = nn.CrossEntropyLoss()
+        self.model = BasicNN( config["n_feats"], config["n_out"], config["dropout_p"] ).to(self.device)
+#        self.criterion = nn.CrossEntropyLoss()
+        self.criterion = nn.BCEWithLogitsLoss()
         self.optimizer = optim.Adam(self.model.parameters(), lr=self.lr)
 
     def get_parameters(self, config): # config not needed at all
@@ -82,7 +92,7 @@ class FlowerClient(fl.client.NumPyClient):
 
     def fit(self, parameters, params):
         print(" ***************************************** FIT self.params.client_id ", self.params)
-        print(f"[Client {self.params.client_id}] fit")
+        print(f"[Client     ] fit")
         self.set_parameters(parameters)
         #train(self.model,self.params,self.dataset)
 # ****** * * * * *  * *  *  *   *   *    *    *  * * * * * * * * ********
@@ -95,6 +105,9 @@ class FlowerClient(fl.client.NumPyClient):
                 
                 # forward
                 logits = self.model(X)
+                logits = logits.squeeze(1)     # [batch]
+                print("client::fit::logits", logits.shape)
+
                 loss = self.criterion(logits, y)
                 
                 # backward
@@ -129,16 +142,19 @@ class FlowerClient(fl.client.NumPyClient):
 
 #    @torch.no_grad()
     def evaluate(self, parameters, params):
-        print(f"^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^[Client {self.params.client_id}] evaluate")
+        print(f"^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^[Client ] evaluate")
         self.set_parameters(parameters)
 # ****** * * * * *  * *  *  *   *   *    *    *  * * * * * * * * ********
         self.model.eval()
         total_loss, correct, total = 0, 0, 0
         
         for X, y in self.test_loader:
+            print("client::evaluate::x,y", X.shape, y.shape)
             X, y = X.to(self.device), y.to(self.device)
             logits = self.model(X)
-            loss = self.criterion(logits, y)
+            #logits = logits.squeeze(1)     # [batch]
+            print("client::evaluate::logits", logits.shape)
+            loss = self.criterion(logits.squeeze(1), y)
             
             total_loss += loss.item() * X.size(0)
             preds = torch.argmax(logits, dim=1)
