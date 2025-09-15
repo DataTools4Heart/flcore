ea1b2349e4af47505419f79c07c8bc16be3175af
Jorge Fabila <jorgefabila1997@gmail.com>
2025-09-15 14:05:34 +0200

    client funcionando

--- flcore/models/nn/client.py
+++ flcore/models/nn/client.py
@@ -35,6 +35,7 @@ from collections import OrderedDict
 from torch.utils.data import TensorDataset, DataLoader
 import torch.optim as optim
 import torch.nn as nn
+import torch.nn.functional as F
 
 from flcore.models.nn.basic_nn import BasicNN
 
@@ -53,16 +54,6 @@ class FlowerClient(fl.client.NumPyClient):
 
         (self.X_train, self.y_train), (self.X_test, self.y_test) = data
 
-        print("ARREGLOS xtrain", self.X_train.shape)
-        print("ARREGLOS ytrain", self.y_train.shape)
-        print("ARREGLOS xtest", self.X_test.shape)
-        print("ARREGLOS ytest", self.y_test.shape)
-
-        print(" xtrain", type(self.X_train))
-        print("ARREGLOS ytrain", type(self.y_train))
-        print("ARREGLOS xtest", type(self.X_test))
-        print("ARREGLOS ytest", self.y_test)
-        
         self.X_train = torch.tensor(self.X_train.values, dtype=torch.float32)
         self.y_train = torch.tensor(self.y_train.values, dtype=torch.float32)
         self.X_test = torch.tensor(self.X_test.values, dtype=torch.float32)
@@ -78,6 +69,20 @@ class FlowerClient(fl.client.NumPyClient):
         self.criterion = nn.BCEWithLogitsLoss()
         self.optimizer = optim.Adam(self.model.parameters(), lr=self.lr)
 
+        if config["n_out"] == 1:  # Binario
+            self.criterion = nn.BCEWithLogitsLoss()
+            #loss = F.binary_cross_entropy_with_logits(logits.squeeze(1), y)
+            """
+            probs = torch.sigmoid(logits.squeeze(1))
+            preds = (probs > 0.5).long()"""
+        else:           # Multiclase
+            self.criterion = nn.CrossEntropyLoss()
+            self.y_train = self.y_train.long()
+            self.y_test = self.y_test.long()
+            #loss = F.cross_entropy(logits, y)
+            #preds = torch.argmax(logits, dim=1)
+        #return loss, preds
+
     def get_parameters(self, config): # config not needed at all
         return [val.cpu().numpy() for _, val in self.model.state_dict().items()]
 
@@ -91,8 +96,6 @@ class FlowerClient(fl.client.NumPyClient):
         self.model.load_state_dict(state_dict, strict=True)
 
     def fit(self, parameters, params):
-        print(" ***************************************** FIT self.params.client_id ", self.params)
-        print(f"[Client     ] fit")
         self.set_parameters(parameters)
         #train(self.model,self.params,self.dataset)
 # ****** * * * * *  * *  *  *   *   *    *    *  * * * * * * * * ********
@@ -102,15 +105,16 @@ class FlowerClient(fl.client.NumPyClient):
             
             for X, y in self.train_loader:
                 X, y = X.to(self.device), y.to(self.device)
-                
-                # forward
                 logits = self.model(X)
-                logits = logits.squeeze(1)     # [batch]
-                print("client::fit::logits", logits.shape)
 
-                loss = self.criterion(logits, y)
-                
-                # backward
+                if self.params["n_out"] == 1:  # Binario
+                    loss = F.binary_cross_entropy_with_logits(logits.squeeze(1), y)
+                    probs = torch.sigmoid(logits.squeeze(1))
+                    preds = (probs > 0.5).long()
+                else:           # Multiclase
+                    loss = F.cross_entropy(logits, y)
+                    preds = torch.argmax(logits, dim=1)
+
                 self.optimizer.zero_grad()
                 loss.backward()
                 self.optimizer.step()
@@ -125,46 +129,49 @@ class FlowerClient(fl.client.NumPyClient):
                 """                
                 # métricas
                 total_loss += loss.item() * X.size(0)
-                preds = torch.argmax(logits, dim=1)
                 correct += (preds == y).sum().item()
                 total += y.size(0)
             
             train_loss = total_loss / total
             train_acc = correct / total
-            test_loss, test_acc = self.evaluate()
+            #test_loss, test_acc = self.evaluate()
             
             print(f"Epoch {epoch+1:02d} | "
-                  f"Train Loss: {train_loss:.4f}, Train Acc: {train_acc:.4f} | "
-                  f"Test Loss: {test_loss:.4f}, Test Acc: {test_acc:.4f}")
+                  f"Train Loss: {train_loss:.4f}, Train Acc: {train_acc:.4f} ")
+            #      f"Test Loss: {test_loss:.4f}, Test Acc: {test_acc:.4f}")
 
-        trainloader_dataset_len = self.dataset.train_size
-        return self.get_parameters(config={}), trainloader_dataset_len, {}
+        dataset_len = self.y_train.shape[0]
+        return self.get_parameters(config={}), dataset_len, {}
 
 #    @torch.no_grad()
     def evaluate(self, parameters, params):
-        print(f"^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^[Client ] evaluate")
         self.set_parameters(parameters)
 # ****** * * * * *  * *  *  *   *   *    *    *  * * * * * * * * ********
         self.model.eval()
         total_loss, correct, total = 0, 0, 0
-        
+
         for X, y in self.test_loader:
-            print("client::evaluate::x,y", X.shape, y.shape)
             X, y = X.to(self.device), y.to(self.device)
             logits = self.model(X)
-            #logits = logits.squeeze(1)     # [batch]
-            print("client::evaluate::logits", logits.shape)
-            loss = self.criterion(logits.squeeze(1), y)
-            
+            if self.params["n_out"] == 1:  # Binario
+                loss = F.binary_cross_entropy_with_logits(logits.squeeze(1), y)
+                probs = torch.sigmoid(logits.squeeze(1))
+                preds = (probs > 0.5).long()
+            else:           # Multiclase
+                loss = F.cross_entropy(logits, y)
+                preds = torch.argmax(logits, dim=1)
+
             total_loss += loss.item() * X.size(0)
             preds = torch.argmax(logits, dim=1)
             correct += (preds == y).sum().item()
             total += y.size(0)
-        return total_loss / total, correct / total
-# ****** * * * * *  * *  *  *   *   *    *    *  * * * * * * * * ********
-#        loss, accuracy = test(self.model, self.dataset)
-#        return float(loss), self.dataset.test_size, {"accuracy": float(accuracy)}
-# ****** * * * * *  * *  *  *   *   *    *    *  * * * * * * * * ********
+
+        test_loss = total_loss / total
+        acc = correct / total
+        dataset_len = self.y_test.shape[0]
+
+#        return total_loss / total, correct / total
+        return float(total_loss), dataset_len, {"accuracy": float(acc)}
 
 def get_client(config,data,client_id) -> fl.client.Client:
 #    client = FlowerClient(params).to_client()
