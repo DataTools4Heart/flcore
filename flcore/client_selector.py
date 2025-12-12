799a3bf32591623578e0bff76b63cf2411f16e2d
Jorge Fabila <jorgefabila1997@gmail.com>
2025-12-12 12:04:24 +0100

    modelos lineales añadidos

--- flcore/client_selector.py
+++ flcore/client_selector.py
@@ -6,10 +6,13 @@ import flcore.models.random_forest as random_forest
 import flcore.models.weighted_random_forest as weighted_random_forest
 import flcore.models.nn as nn
 
+linear_models_list = ["logistic_regression", "linear_regression", "lsvc",
+                      "lasso_regression", "ridge_regression"]
+
+
 def get_model_client(config, data):
     model = config["model"]
-# MODIFICAR EL SELECCIONADOR DE MODELOS AÑADIR LAS DEMAS OPCIONES
-    if model in ("logistic_regression", "elastic_net", "lsvc"):
+    if model in linear_models_list:
         client = linear_models.client.get_client(config,data)
 
     elif model == "random_forest":
