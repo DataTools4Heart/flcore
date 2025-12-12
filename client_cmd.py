ef62c683edf867f099c335a1ade3890f372f457f
Jorge Fabila <jorgefabila1997@gmail.com>
2025-12-12 12:37:46 +0100

    moviendo cosas a utils

--- client_cmd.py
+++ client_cmd.py
@@ -12,9 +12,7 @@ import logging
 #import grpc
 
 import flcore.datasets as datasets
-from flcore.client_selector import get_model_client
-
-# Start Flower client but after the server or error
+from flcore.utils import StreamToLogger, GetModelClient, SanityCheck
 
 if __name__ == "__main__":
 
@@ -123,60 +121,7 @@ if __name__ == "__main__":
         new.append(parsed)
     config["target_labels"] = new
 
-###################### AQUI HAY QUE PONER LO DEL SANITY CHECK, concordancia entre task, modelo, etc
-    """
-    # Compaibilidad de logistic regression y elastic net con sus parámetros
-    if config["model"] == "logistic_regression":
-        if config["penalty"] == "elasticnet":
-            if config["solver"] != "saga":
-                config["solver"] = "saga"
-            if config["l1_ratio"] == 0:
-                print("Degenerate case equivalent to Penalty L1")
-            elif config["l1_ratio"] == 1:
-                print("Degenerate case equivalent to Penalty L2")
-        if config["penalty"] == "L1":
-            if config["l1_ratio"] != 0:
-               config["l1_ratio"] = 0
-            elif config["l1_ratio"] != 1:
-               config["l1_ratio"] = 1                
-            
-        En el sanity check hay que poner que el uncertainty aware es solamente para NN
-        Solvers como 'newton-cg', 'sag', 'lbfgs' — sólo soportan L2 o ninguna penalización. 
-
-        Solvers 'liblinear' — soportan L1 y L2 (pero no elasticnet). 
-
-        Solver 'saga' — soporta L1, L2 y elasticnet, por lo que es el más flexible entre ellos. 
-    # Disponibilidad de clasificación / regresión según el modelo
-
-    if config["model"] in ["lsvc", "logistic_regression"]:
-        if config["task"] == "regression":
-            print("The nature of the selected ML models does not allow to perform regression")
-            print("if you want to perform regression with a linear model you can change to linear regression)
-            # sys.exit()
-    elif config["model"] == "linear_regression":
-        if config["task"] == "classification":
-            print("The nature of the selected ML model does not allow to perform classification")
-            # sys.exit()
-    """
-
-    if config["model"] in ("logistic_regression", "elastic_net", "lsvc"):
-        config["linear_models"] = {}
-        n_feats = len(config["train_labels"])
-        config['linear_models']['n_features'] = n_feats # config["n_features"]
-        config["held_out_center_id"] = -1
-    elif config["model"] == "nn": # in ("nn", "BNN"):
-        config["n_feats"] = len(config["train_labels"])
-        config["n_out"] = 1 # Quizás añadir como parámetro también
-        config["dropout_p"] = config["neural_network"]["dropout_p"]
-        config["device"] = config["neural_network"]["device"]
-        config["batch_size"] = 32
-        config["lr"] = 1e-3
-        config["local_epochs"] = config["neural_network"]["local_epochs"]
-# **************************************************************************************************************
-#    parser.add_argument("--xgb", type=json.loads, default={"batch_size": 32,"num_iterations": 100,"task_type": "BINARY","tree_num": 500}, help="XGB parameters")
-    elif config["model"] == "xgb":
-        pass
-# **************************************************************************************************************
+    SanityCheck()
     # Create sandbox log file path
     sandbox_log_file = Path(os.path.join(config["sandbox_path"], "log_client.txt"))
 
@@ -198,19 +143,6 @@ if __name__ == "__main__":
     logger.handlers = []  # Clear any default handlers
     logger.addHandler(file_handler)
     logger.addHandler(console_handler)
-
-    # Redirect print() and sys.stdout/sys.stderr into logger
-    class StreamToLogger:
-        def __init__(self, logger, level):
-            self.logger = logger
-            self.level = level
-
-        def write(self, message):
-            for line in message.rstrip().splitlines():
-                self.logger.log(self.level, line.rstrip())
-
-        def flush(self):
-            pass
 
     # Create two sub-loggers
     stdout_logger = logging.getLogger("STDOUT")
@@ -265,7 +197,7 @@ num_client = 0 # config["client_id"]
 (X_train, y_train), (X_test, y_test) = datasets.load_dataset(config, num_client)
 
 data = (X_train, y_train), (X_test, y_test)
-client = get_model_client(config, data)
+client = GetModelClient(config, data)
 """
 if isinstance(client, fl.client.NumPyClient):
     fl.client.start_numpy_client(
