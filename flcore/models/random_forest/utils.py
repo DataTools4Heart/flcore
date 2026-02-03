fa7191d3cd413ad975656f8323780cc61a5de4c0
faildeny <grzegorz.skorupko@gmail.com>
2026-02-03 17:26:32 +0100

    Improve Random Forest hyperparameters

--- flcore/models/random_forest/utils.py
+++ flcore/models/random_forest/utils.py
@@ -23,9 +23,9 @@ from typing import cast
 
 def get_model(bal_RF):
     if(bal_RF == True):
-        model = BalancedRandomForestClassifier(n_estimators=100,random_state=42)
+        model = BalancedRandomForestClassifier(n_estimators=300,max_depth=10)
     else:
-        model = RandomForestClassifier(n_estimators=100,class_weight= "balanced",max_depth=2,random_state=42)
+        model = RandomForestClassifier(n_estimators=300,max_depth=10,class_weight= "balanced_subsample")
     
     return model
 
