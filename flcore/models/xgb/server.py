67f20da50e3ea28c3edd83941f9ed8e02b926293
faildeny <grzegorz.skorupko@gmail.com>
2026-02-05 18:01:30 +0100

    Fix missing metrics from XGBoost model in distributed fit

--- flcore/models/xgb/server.py
+++ flcore/models/xgb/server.py
@@ -98,10 +98,13 @@ class FL_Server(fl.server.Server):
         for current_round in range(1, num_rounds + 1):
             # Train model and replace previous global model
             res_fit = self.fit_round(server_round=current_round, timeout=timeout)
-            if res_fit:
-                parameters_prime, _, _ = res_fit  # fit_metrics_aggregated
+            if res_fit is not None:
+                parameters_prime, fit_metrics, _ = res_fit  # fit_metrics_aggregated
                 if parameters_prime:
                     self.parameters = parameters_prime
+                history.add_metrics_distributed_fit(
+                    server_round=current_round, metrics=fit_metrics
+                )
 
             # Evaluate model using strategy implementation
             res_cen = self.strategy.evaluate(current_round, parameters=self.parameters)
